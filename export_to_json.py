3ac2490ed55e4f6633279c8b95559b04c79f2203
Yisrael jacob <yisraeljakob@gmail.com>
2023-06-13 14:26:14 +0300

    oxport to json file

---
+++ export_to_json.py
@@ -0,0 +1,17 @@
+import json
+
+
+def save_dict_to_json(file_path: str, data: list):
+    with open(file_path, "w") as json_file:
+        json.dump(data, json_file, indent=2)
+
+
+def convert_dict_to_json_array(file_path: str, pptx_text_dict: dict, openai_returned_dict: dict):
+    json_array = [{"slide number": key, "original text": pptx_text_dict[key], "explanation": value}
+                  for key, value in openai_returned_dict.items()]
+    save_dict_to_json(file_path, json_array)
+
+
+def write_to_json(file_path: str, pptx_text_dict: dict, openai_returned_dict: dict):
+    file_path = file_path.split('.')[0] + '.json'
+    convert_dict_to_json_array(file_path, pptx_text_dict, openai_returned_dict)
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import time
-
 from extract_text_from_pptx import extract_text_from_pptx
 from openAI_calls import call_openai_api_helper
+from export_to_json import write_to_json
 import asyncio
-
-loop = asyncio.get_event_loop()
 
 
 def main():
-    text_dict = extract_text_from_pptx("Longest Common Subsequence.pptx")
-    returned_dict = asyncio.run(call_openai_api_helper(text_dict))
-    print(returned_dict)
+    pptx_path = input("Hi dear student. \n You missed a lecture in college? Don't worry.\n"
+                      "Enter the path to the pptx file of the lecture and you will get a json file with explanation.")
+    pptx_text_dict = extract_text_from_pptx(pptx_path)
+    openai_returned_dict = asyncio.run(call_openai_api_helper(pptx_text_dict))
+    write_to_json(pptx_path,pptx_text_dict, openai_returned_dict)
 
 
 if __name__ == "__main__":
