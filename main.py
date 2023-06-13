e968eb21d6d69c84ea7045ac17bcb86b4f96e445
Yisrael jacob <yisraeljakob@gmail.com>
2023-06-13 15:00:25 +0300

    documantation and test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import asyncio
 
 def main():
     pptx_path = input("Hi dear student. \n You missed a lecture in college? Don't worry.\n"
-                      "Enter the path to the pptx file of the lecture and you will get a json file with explanation.")
+                      "Enter the path to the pptx file of the lecture and you will get a json file with explanation.\n")
     pptx_text_dict = extract_text_from_pptx(pptx_path)
     openai_returned_dict = asyncio.run(call_openai_api_helper(pptx_text_dict))
     write_to_json(pptx_path,pptx_text_dict, openai_returned_dict)
