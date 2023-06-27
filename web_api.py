00d4ca1cead64773ea3e4a55e287a3a9e14a1ef6
Yisrael jacob <yisraeljakob@gmail.com>
2023-06-27 10:23:06 +0300

    Status function

--- web_api.py
+++ web_api.py
@@ -10,6 +10,15 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 
+def get_file_details(filename):
+    filename = os.path.basename(filename)
+    parts = filename.split('_')
+    original_filename = '_'.join(parts[:-2])
+    timestamp = datetime.strptime(parts[-2], '%Y%m%d%H%M%S')
+    uid = parts[-1].split('.')[0]
+    return original_filename, timestamp, uid
+
+
 @app.route('/', methods=['POST'])
 def upload():
     """
@@ -31,6 +40,37 @@ def upload():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
         return jsonify({'uid': uid})
     return jsonify({'error': 'No file uploaded.'})
+
+
+@app.route('/<uid>', methods=['GET'])
+def status(uid):
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    matching_files = [file for file in files if uid in file]
+    if len(matching_files) == 0:
+        return jsonify({
+            'status': 'not found',
+            'filename': None,
+            'timestamp': None,
+            'explanation': None
+        }), 404
+
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], matching_files[0])
+    original_filename, timestamp, _ = get_file_details(matching_files[0])
+
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as f:
+            explanation = json.load(f)
+        status = 'done'
+    else:
+        explanation = None
+        status = 'pending'
+
+    return jsonify({
+        'status': status,
+        'filename': original_filename,
+        'timestamp': timestamp,
+        'explanation': explanation
+    }), 200
 
 
 if __name__ == '__main__':
