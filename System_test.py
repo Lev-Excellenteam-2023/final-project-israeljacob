83e79052cead2303cad31860e260b03976d20b08
Yisrael jacob <yisraeljakob@gmail.com>
2023-07-04 22:22:40 +0300

    Changed from comments to documentation

--- System_test.py
+++ System_test.py
@@ -1,20 +1,20 @@
 import time
 from client import client
 
-# Base URL of the Web API
 base_url = 'http://127.0.0.1:5000'
 
-# Path of the sample presentation to upload
 sample_presentation_path = 'C:/Users/yisra/Desktop/to_pass/study/lev/year 3/tt.pptx'
 
 
 def run_system_test():
-    # Upload the sample presentation
+    """
+    Upload the sample presentation and then waits for the file processing to complete.
+    When it completes, it prints the explanation
+    """
     print("Uploading the sample presentation...")
     uid = client.upload_file(base_url, sample_presentation_path)
     print("Sample presentation uploaded. UID:", uid)
 
-    # Wait for the file processing to complete
     while True:
         status = client.get_status(base_url, uid)
         if client.is_done(status):
@@ -22,7 +22,6 @@ def run_system_test():
         print("File processing is still in progress. Waiting...")
         time.sleep(5)
 
-    # File processing is done
     print("File processing is complete.")
     print("Status:", status['status'])
     print("Original Filename:", status['filename'])
